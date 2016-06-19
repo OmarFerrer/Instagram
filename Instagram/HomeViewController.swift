a18df5773a74ebb7fde67b0da2d22fefb3114a73
bc0067042 <bc0067042@ishi-pc20192992.local>
2016-06-19 14:45:48 +0900

    update 課題の手前

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -17,8 +17,8 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     var postArray: [PostData] = []
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
         let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
         tableView.registerNib(nib, forCellReuseIdentifier: "Cell")
@@ -66,6 +66,62 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         })
         
         
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+//        
+//        let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+//        tableView.registerNib(nib, forCellReuseIdentifier: "Cell")
+//        tableView.rowHeight = UITableViewAutomaticDimension
+//        
+//        //要素が追加されたらArrayに追加してテーブル再表示
+//        FIRDatabase.database().reference().child(CommonConst.PostPATH).observeEventType(.ChildAdded, withBlock: {snapshot in
+//            
+//            if let uid = FIRAuth.auth()?.currentUser?.uid {
+//                //Postdataクラスを生成して受け取ったデータを設定
+//                let postData = PostData(snapshot: snapshot, myId: uid)
+//                self.postArray.insert(postData, atIndex: 0)
+//                
+//                //テーブル再表示
+//                self.tableView.reloadData()
+//            }
+//        })
+//        
+//        //要素が変更されたら当該データを削除後追加して再表示
+//        FIRDatabase.database().reference().child(CommonConst.PostPATH).observeEventType(.ChildChanged, withBlock: {snapshot in
+//            
+//            //Postdataクラスを生成して受け取ったデータを設定
+//            if let uid = FIRAuth.auth()?.currentUser?.uid {
+//                let postData = PostData(snapshot: snapshot, myId: uid)
+//                
+//                //保持している配列から同じIDを探す
+//                var index: Int = 0
+//                for post in self.postArray {
+//                    if post.id == postData.id{
+//                        index = self.postArray.indexOf(post)!
+//                        break
+//                    }
+//                }
+//                
+//                //差し替えるため一度削除
+//                self.postArray.removeAtIndex(index)
+//                
+//                //更新済データを登録
+//                self.postArray.insert(postData, atIndex: index)
+//                
+//                //当該セルのみ再表示
+//                let indexPath = NSIndexPath(forRow: index, inSection: 0)
+//                self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.None)
+//            }
+//        })
+//        
+//        
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -79,13 +135,13 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.postData = postArray[indexPath.row]
         
         //セル内ボタンのアクションをソースコードで設定
-        cell.likeButton.addTarget(self, action: "handleButton:event:", forControlEvents: UIControlEvents.TouchUpInside) //★この記述の意味？
+        cell.likeButton.addTarget(self, action: #selector(self.handleButton), forControlEvents: UIControlEvents.TouchUpInside) //★この記述の意味？
         //UILabelの行数が変わっている可能性があるので再描画
         cell.layoutIfNeeded()
         
         return cell
     }
-
+    
     func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         //auto layoutを使ってセルの高さを動的に変化
         return UITableViewAutomaticDimension
@@ -138,5 +194,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             
         }
     }
+    
 }
 
